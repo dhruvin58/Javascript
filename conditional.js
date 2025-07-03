acb5ceddd4408f41be971a10561484c8c77b30f7
Dhruvin Mavani <dhruvinmavani3302@gmail.com>
2025-07-03 21:25:20 +0530

    conditional statement practice

---
+++ conditional.js
@@ -0,0 +1,13 @@
+let age = 22;
+
+let ans = age >= 18 ? "Can Drive" : "Cannot drive";
+
+console.log(ans);
+
+let istrue = false;
+
+istrue && console.log("Welcome back to the session");
+
+let username = "Dhruvin Mavani";
+let name = username || "Guest";
+console.log(name);
